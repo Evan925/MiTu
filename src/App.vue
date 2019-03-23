bbc4e09adedb8c07d583abe26bdc7fe049de50b2
tzxiaomo <1431128779@qq.com>
2019-03-23 21:27:11 +0800

    添加iviewUI 插件

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@
     <img src="./assets/logo.png">
     <div>
       <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
+        If iView is successfully added to this project, you'll see an
+        <code v-text="'<Button>'"></code>
         below
       </p>
-      <el-button>el-button</el-button>
+      <Button type="primary">Button</Button>
     </div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
--- src/main.js
+++ src/main.js
@@ -2,13 +2,21 @@ import Vue from 'vue'
 import './plugins/axios'
 import App from './App.vue'
 import router from './router'
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+// import iView from 'iview'
+// import 'iview/dist/styles/iview.css'
+
 import store from './store'
 import './plugins/element.js'
+import './plugins/iview.js'
+// Vue.use(ElementUI)
+// Vue.use(iView)
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
